e2f41951ac31b31e94eeae1d48904c7c7a9c214a
Andrew Kennedy <andrew@umsl.co.uk>
2020-11-11 01:00:38 +0000

    extended tests

--- tests/palidromes.test.js
+++
@@ -1,8 +0,0 @@
-const palidromes = require('../palidromes');
-
-describe('palidromes()', () => {
-  it('correctly identifies one-word palidromes', () => {
-    expect(palidromes('madam')).toEqual(['madam']);
-    expect(palidromes('racecar')).toEqual(['racecar']);
-  });
-});
---
+++ wallaby.js
@@ -0,0 +1,12 @@
+module.exports = function () {
+  return {
+    testFramework: 'jest', //(1)
+
+    env: {
+      type: 'node', //(2)
+    },
+
+    tests: ['tests/**/*.test.js'], //(3)
+    files: ['**/*.js', '!node_modules/**/*', '!**/*.test.js', '!**/.*'], //(4)
+  };
+};
